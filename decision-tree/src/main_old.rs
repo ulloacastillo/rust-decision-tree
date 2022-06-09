c3f539824cb8dda94c42e60e4c58b85eaef2ba76
ulloacastillo <jorgeulloacastillo@gmail.com>
2022-06-09 15:15:29 -0400

    making predictions

--- decision-tree/src/main_old.rs
+++ decision-tree/src/main_old.rs
@@ -3,29 +3,29 @@ use std::io::prelude::*;
 
 
 use csv::Error;
-#[derive(Debug)]
-struct BestSplitStruct {
+
+struct BestSplitStruct <'d> {
     feature_index: usize,
     threshold: f32,
     dataset_left: Vec<Vec<f32>>,
     dataset_right: Vec<Vec<f32>>,
-    y_right: Vec<String>,
-    y_left: Vec<String>,
+    y_right: Vec<&'d str>,
+    y_left: Vec<&'d str>,
     info_gain: f32
 }
 
 #[derive(Debug)]
-struct Node {
-    left: Option<Box<Node>>,
-    right: Option<Box<Node>>,
+struct Node <'a>{
+    left: Option<Box<Node<'a>>>,
+    right: Option<Box<Node<'a>>>,
     feature_index: usize,
     threshold: f32,
     //for leaf Nodes
-    value: String
+    value: &'a str
 }
 
-impl Node{
-    pub fn new(fi: usize, th: f32, v: String) -> Self {
+impl <'a> Node <'a>{
+    pub fn new(fi: usize, th: f32, v: &'a str) -> Self {
         Node {
             left: None,
             right: None,
@@ -36,18 +36,22 @@ impl Node{
     }
 }
 
+impl <'g> From<Node <'g>> for Option<Box<Node <'g>>> {
+    fn from(node: Node<'g>) -> Self {
+        Some(Box::new(node))
+    }
+}
 
-#[derive(Debug)]
-struct DecisionTreeClassifier {
-    root: Option<Box<Node>>,
+struct DecisionTreeClassifier <'a>{
+    root: Option<Box<Node<'a>>>,
 
     // stopping conditions
     min_samples_split: usize,
     max_depth: usize
 }
 
-impl  DecisionTreeClassifier  {
-    pub fn new(mss: usize, md: usize) -> Self{
+impl <'a> DecisionTreeClassifier <'a> {
+    pub fn new(&mut self, mss: usize, md: usize) -> Self{
         DecisionTreeClassifier {
             root: None,
             min_samples_split: mss,
@@ -55,30 +59,27 @@ impl  DecisionTreeClassifier  {
         }
     }
 
-    pub fn build_tree(&mut self, X: &Vec<Vec<f32>>, Y: &Vec<String>, curr_depth: usize) -> Option<Box<Node>>{
+    pub fn build_tree<'h>(&mut self, X: &Vec<Vec<f32>>, Y: &'h Vec<&str>, curr_depth: usize) -> Option<Box<Node<'h>>>{
         let num_samples = X.len();
         let num_features = X[0].len();
 
         if num_samples >= self.min_samples_split && curr_depth <= self.max_depth {
             let best_split: BestSplitStruct = self.get_best_split(X, Y, num_samples, num_features);
-            println!("{:?}", best_split);
             if best_split.info_gain > 0.0 {
                 let left_subtree = DecisionTreeClassifier::build_tree(self, &best_split.dataset_left, &best_split.y_left, curr_depth+1);
                 let right_subtree = DecisionTreeClassifier::build_tree(self, &best_split.dataset_right, &best_split.y_right, curr_depth+1);
                 
-                //println!("{:?}", left_subtree);
-
                 return Some(Box::new(Node {
                     left: left_subtree,
                     right: right_subtree,
                     feature_index: best_split.feature_index,
                     threshold: best_split.threshold,
-                    value: "".to_string()
+                    value: ""
                 }));
             }
 
         }
-        let leaf_value: String = self.calculate_leaf_value(Y);
+        let leaf_value: &str = self.calculate_leaf_value(Y);
 
         return Some(Box::new(Node {
             left: None,
@@ -89,7 +90,7 @@ impl  DecisionTreeClassifier  {
         }));
     }
 
-    pub fn get_best_split(&mut self, X: &Vec<Vec<f32>>, Y:  &Vec<String>, num_samples: usize, num_features: usize) -> BestSplitStruct{
+    pub fn get_best_split<'f>(&mut self, X: &Vec<Vec<f32>>, Y: &'f Vec<&'f str>, num_samples: usize, num_features: usize) -> BestSplitStruct<'f>{
         let mut best_split = BestSplitStruct {
             feature_index: 0,
             threshold: 0.0,
@@ -107,13 +108,13 @@ impl  DecisionTreeClassifier  {
             let possible_thresholds = unique_vals_f32(&feature_values);
 
             for &threshold in possible_thresholds.iter() {
-                let dataset_splitted: (Vec<Vec<f32>>, Vec<Vec<f32>>, Vec<String>, Vec<String>) = self.split(X, Y, feature_index, threshold);
+                let dataset_splitted: (Vec<Vec<f32>>, Vec<Vec<f32>>, Vec<&str>, Vec<&str>) = self.split(X, Y, feature_index, threshold);
                 let dataset_left: Vec<Vec<f32>> = dataset_splitted.0;
                 let dataset_right: Vec<Vec<f32>> = dataset_splitted.1;
                 
                 if dataset_left.len() > 0 && dataset_right.len() > 0 {
-                    let y_left: Vec<String> = dataset_splitted.2;
-                    let y_right: Vec<String> = dataset_splitted.3;
+                    let y_left: Vec<&str> = dataset_splitted.2;
+                    let y_right: Vec<&str> = dataset_splitted.3;
 
                     let curr_info_gain = self.information_gain(Y, &y_left, &y_right);
 
@@ -130,44 +131,39 @@ impl  DecisionTreeClassifier  {
                 }
             }
         }
-        //println!("{:?}", best_split);
         best_split
     }
 
-    pub fn gini_index(&mut self, Y: &Vec<String>) -> f32 {
-        let class_labels = unique_vals(&Y);
-        
+    pub fn gini_index(&mut self, Y: &Vec<&str>) -> f32 {
+        let class_labels = unique_vals(Y);
         let mut gini = 0.0;
         
         for cls in class_labels.iter() {
-            
-            let p_cls: f32 = (((count_vals(&Y, cls.to_string()) as i32) as f32) / (Y.len() as i32) as f32) as f32;
-            
-            
+            let p_cls: f32 = (count_vals(&Y, &cls) / Y.len()) as f32;
             gini = gini + (p_cls * p_cls);
 
         }
         gini
     }
 
-    pub fn split(&mut self, X: &Vec<Vec<f32>>, Y: &Vec<String>, feature_index: usize, threshold: f32) -> (Vec<Vec<f32>>, Vec<Vec<f32>>, Vec<String>, Vec<String>){
+    pub fn split<'b>(&mut self, X: &Vec<Vec<f32>>, Y: &'b Vec<&str>, feature_index: usize, threshold: f32) -> (Vec<Vec<f32>>, Vec<Vec<f32>>, Vec<&'b str>, Vec<&'b str>){
         let mut dataset_left: Vec<Vec<f32>> = vec![];
         let mut dataset_right: Vec<Vec<f32>> = vec![];
-        let mut y_right: Vec<String> = vec![];
-        let mut y_left: Vec<String> = vec![];
+        let mut y_right: Vec<&str> = vec![];
+        let mut y_left: Vec<&str> = vec![];
 
 
         for i in 0..X.len() {
             let v: Vec<f32> = X[i].to_vec();
-            let v_y = &Y[i];
+            let v_y: &str = Y[i];
             if v[feature_index] <= threshold {
                 dataset_left.push(v);
-                y_left.push(v_y.to_string());
+                y_left.push(v_y);
 
             }
             else {
                 dataset_right.push(v);
-                y_right.push(v_y.to_string());
+                y_right.push(v_y);
             }
         }
 
@@ -175,18 +171,18 @@ impl  DecisionTreeClassifier  {
 
     }
 
-    pub fn information_gain(&mut self, parent: &Vec<String>, l_child: &Vec<String>, r_child: &Vec<String>) -> f32{
+    pub fn information_gain(&mut self, parent: &Vec<&str>, l_child: &Vec<&str>, r_child: &Vec<&str>) -> f32{
         let weight_l: f32 = (l_child.len() / parent.len()) as f32;
         let weight_r: f32 = (r_child.len() / parent.len()) as f32;
 
         let gain: f32 = self.gini_index(parent) - (weight_l*self.gini_index(l_child) + weight_r*self.gini_index(r_child));
-        //println!("gini: {}", self.gini_index(parent));
+
         gain
 
     }
 
-    pub fn calculate_leaf_value(&mut self, Y: &Vec<String> ) -> String{
-        let uni_vals: Vec<String> = unique_vals(Y);
+    pub fn calculate_leaf_value<'c>(&mut self, Y: &'c Vec<&str> ) -> &'c str{
+        let uni_vals: Vec<&str> = unique_vals(Y);
         let mut counts: Vec<usize> = vec![0; uni_vals.len()];
 
         for i in 0..uni_vals.len() {
@@ -206,50 +202,22 @@ impl  DecisionTreeClassifier  {
                 max_idx = i;
             }
         }
-        uni_vals[max_idx].to_string()
-    }
-
-    pub fn fit(&mut self, X: &Vec<Vec<f32>>, Y: &Vec<String>) {
-        self.root = self.build_tree(&X, &Y, 0);
-    }
-
-    pub fn make_prediction(&self, X: &Vec<f32>, tree: &Option<Box<Node>>) -> String{
-        
-        if tree.as_ref().unwrap().value != "" {
-            return tree.as_ref().unwrap().value.to_string();
-        }
-
-        
-
-        let idx:usize = tree.as_ref().unwrap().feature_index;
-        let feature_val = X[idx];
-
-        if feature_val<= tree.as_ref().unwrap().threshold {
-            let sub_tree_l = &tree.as_ref().unwrap().left;
-            return self.make_prediction(X, &sub_tree_l);
-        }
-
-        else {
-            let sub_tree_r = &tree.as_ref().unwrap().right;
-            return self.make_prediction(X, &sub_tree_r);
-        }
-
-        
+        uni_vals[max_idx]
     }
 
 }
 
-fn unique_vals(arr: &Vec<String>) -> Vec<String> {
-    let mut u_vals: Vec<String> = vec![];
+fn unique_vals<'a>(arr: &'a Vec<&str>) -> Vec<&'a str> {
+    let mut u_vals: Vec<&str> = vec![];
     for el in arr.iter() {
         if !u_vals.contains(&el) {
-            u_vals.push(el.to_string());
+            u_vals.push(el);
         }
     }
     u_vals
 }
 
-fn unique_vals_f32(arr: &Vec<f32>) -> Vec<f32> {
+fn unique_vals_f32<'a>(arr: &'a Vec<f32>) -> Vec<f32> {
     let mut u_vals: Vec<f32> = vec![];
     for el in arr.iter() {
         if !u_vals.contains(&el) {
@@ -259,14 +227,13 @@ fn unique_vals_f32(arr: &Vec<f32>) -> Vec<f32> {
     u_vals
 }
 
-fn count_vals(arr: &Vec<String>, label: String) -> usize {
+fn count_vals(arr: &Vec<&str>, label: &str) -> usize {
     let mut c = 0;
     for el in arr.iter() {
         if el == &label {
             c = c + 1;
         }
     }
-    
     c
 }
 
@@ -275,7 +242,7 @@ fn get_column(matrix: &Vec<Vec<f32>>, col: usize) -> Vec<f32>{
     for row in matrix.iter() {
         for (j, &colu) in row.iter().enumerate() {
             if j == col {
-                //println!("{} {}", j, col);
+                println!("{} {}", j, col);
                 column.push(colu);
             }
         }
@@ -308,17 +275,16 @@ fn main() -> Result<(), Error> {
 
 
     // Call split, and use collect() to get a string Vec.
-    //let strings: Vec<String> = [ "a", "b", "c" ].iter().map(|&s|s.into()).collect();
-    /*let values: Vec<String> = content.split('\n').collect();
-    let mut y: Vec<String> = vec![];
+    let values: Vec<&str> = content.split('\n').collect();
+    let mut y: Vec<&str> = vec![];
     let mut x: Vec<Vec<f32>> = vec![];
 
     // Pretty-print the results.
-    let _xs: Vec<String> = values[0].split(',').collect();
+    let _xs: Vec<&str> = values[0].split(',').collect();
     
 
     for _row in values.iter() {
-        let xs: Vec<String> = _row.split(',').collect();
+        let xs: Vec<&str> = _row.split(',').collect();
         
         let mut aux: Vec<f32> = vec![];
         for i in 0..xs.len()-1{
@@ -326,44 +292,24 @@ fn main() -> Result<(), Error> {
         }
         y.push(xs[xs.len() -1]);
         x.push(aux);
-    }
-    */
-
-    let values: Vec<String> = content.split('\n').map(str::to_string).collect();
-    let mut y: Vec<String> = vec![];
-    let mut x: Vec<Vec<f32>> = vec![];
-
-    // Pretty-print the results.
-    let _xs: Vec<String> = values[0].split(',').map(str::to_string).collect();
-    
-
-    for _row in values.iter() {
-        let xs: Vec<String> = _row.split(',').map(str::to_string).collect();
         
-        let mut aux: Vec<f32> = vec![];
-        for i in 0..&xs.len()-1{
-            aux.push(xs[i].parse::<f32>().unwrap());
-        }
-        let a = &xs.len() -1;
-        let b = &xs[a];
-        &y.push(b.to_string());
-        &x.push(aux);
+
     }
 
+    for _row in x.iter(){
+        //println!("VALUES: {:?}", _row);
+        continue;
+    }
+    
+    let a = 4;
+    let b = 2.0;
 
-
+    let n = Node::new(a, b, "xd");
+    println!("VALUES: {:?}", n);
+    
     let aa_ = get_column(&x, 1);
 
-    let mut tree_classifier = DecisionTreeClassifier::new(3, 3);
-
-    tree_classifier.fit(&x, &y);
-
-    
-    let x_i = vec![5.1,3.5,1.4,0.2];
-
-    let prediction = tree_classifier.make_prediction(&x_i, &tree_classifier.root);
-
-    println!("prediction: {}", prediction);
+    println!("VALUES: {:?}", aa_);
 
     Ok(())
     
